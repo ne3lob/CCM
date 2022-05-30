a1791c3e8dcf1b3d0c9f7c0945cdb1440f12297e
Dock11 <grustidrug@gmail.com>
2022-05-30 18:12:02 +0200

    Tutorial Sounds / Audio Manager + Random Point Colors at start + Point catch mechanic done

--- Assets/CMM/Scripts/Punkte.cs
+++ Assets/CMM/Scripts/Punkte.cs
@@ -5,30 +5,55 @@ using UnityEngine;
 public class Punkte : MonoBehaviour
 {
     [SerializeField] private GameObject[] points;
+    private List<GameObject> toEnable = new List<GameObject>();
+    [SerializeField] private Material[] pickMaterial;
+
+    public bool pickPoints;
     private GameObject point;
     private int index;
-    private int pointsOnCount;
+    private int pointsOn_Count;
     
     // Start is called before the first frame update
     void Start()
     {
-        pointsOnCount = 2;
+        pointsOn_Count = 2;
         foreach (var p in points)
         {
-            p.GetComponent<MeshRenderer>().enabled = false;
-        }
+            toEnable.Add(p);
+            p.GetComponent<MeshRenderer>().material = pickMaterial[Random.Range(0, pickMaterial.Length)];
 
+            //p.GetComponent<MeshRenderer>().enabled = false;
+        }
+        Debug.Log(toEnable.Count);
         index = Random.Range(0, points.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        LightOn();
+        if (pickPoints)
+        {
+            LightOn();
+        }
+        
+        DisablePoints();
     }
 
     private void LightOn()
     {
+        GameObject pickPoint = toEnable[Random.Range(0, toEnable.Count)];
+        pickPoint.SetActive(true);
+        toEnable.Remove(pickPoint);
+        pickPoints = false;
         
+        Debug.Log(toEnable.Count);
+    }
+
+    private void DisablePoints()
+    {
+        foreach (var p in toEnable)
+        {
+            p.SetActive(false);
+        }
     }
 }
---
+++ Assets/CMM/Sound/AudioManager/Sound.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound : MonoBehaviour
+{
+    [SerializeField] 
+    private List<AudioClip> speech;
+
+    private AudioClip nextClip;
+    private int clipIndex;
+
+    private AudioSource aSource;
+    
+    [SerializeField] 
+    private Punkte punkteScript;
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        clipIndex = 0;
+        //aSource.clip = speech[clipIndex];
+        //aSource.Play();
+
+        StartCoroutine(playSounds());
+    }
+
+    private void Update()
+    {
+        //PlayNext();
+    }
+
+    private void PlayNext()
+    {
+        aSource.clip = speech[clipIndex];
+        aSource.Play();
+        if (!aSource.isPlaying)
+        {
+            clipIndex++;
+        }
+        
+    }
+
+ 
+    IEnumerator playSounds()
+    {
+        aSource.clip = speech[clipIndex];
+        aSource.Play();
+        yield return new WaitForSeconds(aSource.clip.length + 1f);
+        
+        clipIndex++;
+        aSource.clip = speech[clipIndex];
+        Debug.Log("Next Sound: " + aSource.clip.name);
+        aSource.Play();
+        yield return new WaitForSeconds(aSource.clip.length + 3f);
+        
+        punkteScript.pickPoints = true;
+        clipIndex++;
+        aSource.clip = speech[clipIndex];
+        Debug.Log("Next Sound: " + aSource.clip.name);
+        aSource.Play();
+    }
+}
